d92c1d54e9d3418ad0c57edb21c7b42465088da5
PranavKdagadakai <pkdagadakai@gmail.com>
2025-07-27 00:37:49 +0530

    2025-07-27 0037-hrs

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -7,7 +7,17 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal right<CR>", {})
 		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+
+    -- Neo-tree setup with hidden files shown
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,      -- Show hidden files (dotfiles)
+          hide_dotfiles = false,
+        },
+      },
+    })
 	end,
 }
